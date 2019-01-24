eb47a0f81bde7b6448506bcb1fe88c037990ac50
aurochs <aurochs@yuannew.net>
2019-01-24 14:50:47 +0800

    03

---
+++ c/e2e03/tc.h
@@ -0,0 +1,26 @@
+
+// C 程序主要包括以下部分：
+// 预处理器指令
+// 函数
+// 变量
+// 语句 & 表达式
+// 注释
+
+#include <stdio.h>
+
+int main()
+{
+   printf("Hello, World! \n");
+   return 0;
+}
+
+/*
+    #include <stdio.h> 是预处理器指令，告诉 C 编译器在实际编译之前要包含 stdio.h 文件。
+    int main() 是主函数，程序从这里开始执行。
+    printf(...) 是 C 中另一个可用的函数，会在屏幕上显示消息 "Hello, World!"。
+    return 0; 终止 main() 函数，并返回值 0。
+
+    $ gcc hello.c
+    $ ./a.out
+*/
+
